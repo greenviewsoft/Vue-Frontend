1cd695c98caa41375320ef677d1c97d4b26560f5
MD TIPU SULTAN <greenviewsoft@gmail.com>
2023-10-30 01:28:21 +0600

    all code added git

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup>
+// import Header from "@/components/Header.vue";
+import { Header, NavBar , Footer, CartSidebar, MobileMenu, NewsLetter} from "@/components";
+</script>
+<template>
+  <div>
+    <div class="backdrop"></div>
+    <a class="backtop fas fa-arrow-up" href="#"></a>
+
+    <!-- Header Section  -->
+    <Header />
+
+    <NavBar />
+
+    <CartSidebar />
+
+    <MobileMenu />
+
+    <!-- main content -->
+
+    <router-view></router-view>
+
+    <NewsLetter />
+
+    <Footer />
+  </div>
+</template>
---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+export { default as NavBar } from "./NavBar.vue";
+export { default as Header } from "./Header.vue";
+export { default as Footer } from "./Footer.vue";
+export { default as CartSidebar } from "./CartSidebar.vue";
+export { default as MobileMenu } from "./MobileMenu.vue";
+export { default as NewsLetter } from "./NewsLetter.vue";
